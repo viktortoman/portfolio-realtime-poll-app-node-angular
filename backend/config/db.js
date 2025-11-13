3879bf9e85bbe4009bacc656926ce492382722e2
Tomán Viktor <viktor.toman19@gmail.com>
2025-11-13 17:08:27 +0100

    Enables MongoDB authentication
    Configures the backend to connect to MongoDB using a username and password.
    This enhances security by requiring authentication for database access.
    Also, adds a healthcheck to the mongo service in docker-compose.yml
    
    The database connection string in `db.js` now includes the username,
    password, host, port, and database name, retrieved from environment
    variables.  The docker-compose file now requires MONGO_ROOT_USER and
    MONGO_ROOT_PASSWORD.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,3 @@
-// backend/config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
@@ -6,13 +5,14 @@ const connectDB = async () => {
     const host = process.env.MONGO_HOST;
     const port = process.env.MONGO_PORT;
     const dbName = process.env.MONGO_DB_NAME;
+    const user = process.env.MONGO_USER;
+    const pass = process.env.MONGO_PASS;
 
-    if (!host || !port || !dbName) {
+    if (!host || !port || !dbName || !user || !pass) {
       throw new Error('Database connection environment variables are not defined.');
     }
 
-    const mongoURI = `mongodb://${host}:${port}/${dbName}`;
-
+    const mongoURI = `mongodb://${user}:${pass}@${host}:${port}/${dbName}?authSource=admin`;
     const conn = await mongoose.connect(mongoURI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
